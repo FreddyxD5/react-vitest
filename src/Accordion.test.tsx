e69feb2b49312fd896897252bbdba8e70e395ce3
FreddyxD5 <freddyxd5@gmail.com>
2023-02-14 21:34:21 -0500

    Added fireEvent to trigger a click on a button

--- src/Accordion.test.tsx
+++ src/Accordion.test.tsx
@@ -1,28 +1,37 @@
 import {describe, expect, test} from 'vitest';
-import {render, screen} from '@testing-library/react'
+import {fireEvent, waitFor, render, screen} from '@testing-library/react'
+import "@testing-library/jest-dom"
+
+import { act } from '@testing-library/react';
+
 import Accordion from './Accordeon.test';
 
 describe("Accordion", ()=>{
     beforeEach(()=>{
-        render(<Accordion  title="Testing"><h4>Someday</h4></Accordion>)
+        render(<Accordion  title="Jesusu"><h4>Holitas</h4></Accordion>)
     });
     
 
     test("Deberia mostrar el titulo todo el tiempo", ()=>{
-        expect(screen.getByText(/Testing/i)).toBeDefined();        
+        expect(screen.getByText(/Jesusu/i)).toBeInTheDocument();        
     })
 
     test("No Deberia mostrar el contenido al inicio", ()=>{
-        expect(screen.getByText(/Content/i)).toBeUndefined()
+        expect(screen.queryByText(/Holitas/i)).not.toBeInTheDocument()
     })
     
+    test("Deberia mostrar el contenido de la funcion acordion click", async()=>{
+        const title = screen.getByText(/Show/i);
+        fireEvent.click(title)
+        expect( await screen.findByText(/Holitas/i)).toBeInTheDocument();
+    })
 
 })
 
 describe("Accordion Test",()=>{
     test("Deberia mostrar el titulo ", ()=>{
-        render(<Accordion title="Jesusu"><h4>Content</h4> </Accordion>)
-        expect(screen.getByText(/Content/i)).toBeDefined()
+        render(<Accordion title="Jesusu"><h4>Content</h4> </Accordion>)        
+        expect(screen.getByText(/Show/i)).toBeDefined()
         expect(screen.getByText(/Jesusu/i)).toBeDefined()
     })
 })
